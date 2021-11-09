334534b9586584e44e9f2d83b0c820152aa862ca
Gerson-Law <gerson.canessa20@itca.edu.sv>
2021-11-09 12:10:54 -0600

    Cambios en fragmentos

--- app/src/main/java/com/itca/crud_proyecto_final/ui/home/HomeViewModel.java
+++ app/src/main/java/com/itca/crud_proyecto_final/ui/home/HomeViewModel.java
@@ -10,14 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("App con Acceso a Base de datos Firebase, MySQl Webhost000 " +
-                "Módulo: Desarrollo de Aplicaciones Usando Tecnologías Emergentes" +
-                "Desarolladores:" +
-                "Heidy  Yohamy Mejia Mundo" +
-                "Christy Alexandra Garcia Calderon" +
-                "Carmen Elena Abarca Perez" +
-                "Docente: Manuel de Jesús Gámez López" +
-                "Segundo Año de Sistemas Informáticos");
+        mText.setValue("CRUD Productos");
     }
 
     public LiveData<String> getText() {
