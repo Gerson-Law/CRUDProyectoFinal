c7976d953e6129c1743cc9fb49db388f28cc3fe6
Gerson-Law <gerson.canessa20@itca.edu.sv>
2021-11-09 10:44:59 -0600

    Modificacion del contenedor

--- app/src/main/java/com/itca/crud_proyecto_final/login1.java
+++ app/src/main/java/com/itca/crud_proyecto_final/login1.java
@@ -2,13 +2,29 @@ package com.itca.crud_proyecto_final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login1 extends AppCompatActivity {
+
+    private Button btn1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login1);
+
+        btn1 = findViewById(R.id.iniciosesion);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent home = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(home);
+            }
+        });
     }
 }
--- app/src/main/java/com/itca/crud_proyecto_final/ui/gallery/GalleryViewModel.java
+++ app/src/main/java/com/itca/crud_proyecto_final/ui/gallery/GalleryViewModel.java
@@ -10,7 +10,7 @@ public class GalleryViewModel extends ViewModel {
 
     public GalleryViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mText.setValue("CRUD Tabla Categorias");
     }
 
     public LiveData<String> getText() {
