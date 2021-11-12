7ea01566729a3f20107339a94513842a68244a0e
Gerson-Law <gerson.canessa20@itca.edu.sv>
2021-11-12 10:44:04 -0600

    Crar usuario

--- app/src/main/java/com/itca/crud_proyecto_final/Registrar.java
+++ app/src/main/java/com/itca/crud_proyecto_final/Registrar.java
@@ -3,12 +3,34 @@ package com.itca.crud_proyecto_final;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class Registrar extends AppCompatActivity {
+
+    EditText nombreco,coreeo,contra1,telefono;
+    Button crear;
+    FirebaseAuth FAuth;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
+
+
+        nombreco = findViewById(R.id.nombreco);
+        coreeo = findViewById(R.id.coreeo);
+        contra1 = findViewById(R.id.contra1);
+        telefono = findViewById(R.id.telefono);
+        crear = findViewById(R.id.crear);
+        nombreco = findViewById(R.id.nombreco);
+
+        FAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
     }
 }
